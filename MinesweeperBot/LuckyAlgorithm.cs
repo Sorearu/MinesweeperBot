b6e42ae5bf96bc2381c87562e499b62a5206105a
Sorearu <andytang247@gmail.com>
2016-08-04 06:21:11 +1200

    Implementation of LuckyAlgorithm

--- MinesweeperBot/LuckyAlgorithm.cs
+++ MinesweeperBot/LuckyAlgorithm.cs
@@ -15,12 +15,45 @@ namespace MinesweeperBot
 
         protected override void FindSafeTiles()
         {
+            CalculateEffectiveNumber();
+            int tileCount = 0;
+            Tile[] tilePool = new Tile[msGame.Height * msGame.Width];
+
             // Iterate over gamefield
-            // Find effective ones
-            // See if there are only 2 unclicked tiles
-            // If so, add those tiles to pool
+            for (int i = 0; i < msGame.Height; i++)
+            {
+                for (int j = 0; j < msGame.Width; j++)
+                {
+                    // Find effective ones
+                    Tile currentTile = msGame.Get(i, j);
+                    if (currentTile.EffectiveNumber == 1)
+                    {
+                        // See if there are only 2 unclicked tiles
+                        Tile[] surroundingTiles = GetSurroundingTiles(i, j);
+                        if (CountSurroundingUnclickedTiles(surroundingTiles) == 2)
+                        {
+                            // If so, add those tiles to pool
+                            foreach (Tile tile in surroundingTiles)
+                            {
+                                if (tile.Type == TileType.Unclicked) { tilePool[tileCount++] = tile; }
+
+                            }                           
+                        }
+                    }
+                }
+            }
+
             // After loop, click random tile from pool
-            // test
+
+            if (tileCount > 0)
+            {
+                Random rng = new Random();
+                Tile luckyTile = tilePool[rng.Next(0, tileCount)];
+                Mouse.MoveToAndClick(luckyTile.X, luckyTile.Y, MouseButton.Left);
+                msGame.Update();
+            }
+
+            msGame.State = State.Simple;
         }
     }
 }
