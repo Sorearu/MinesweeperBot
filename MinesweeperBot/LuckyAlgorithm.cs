551d81538a83fd5f439569d91a0dcf87c263894e
Sorearu <andytang247@gmail.com>
2016-08-30 18:56:55 +1200

    finished expanding guessing algorithm

--- MinesweeperBot/LuckyAlgorithm.cs
+++ MinesweeperBot/LuckyAlgorithm.cs
@@ -12,48 +12,63 @@ namespace MinesweeperBot
         protected override void FindFlags()
         {
         }
-        // jello
+
         protected override void FindSafeTiles()
         {
             CalculateEffectiveNumber();
             int tileCount = 0;
             Tile[] tilePool = new Tile[msGame.Height * msGame.Width];
-
-            // Iterate over gamefield
-            for (int i = 0; i < msGame.Height; i++)
+        
+            // offset refers to how many surrounding unclicked tiles we want to check for in relation
+            // to the effective tile number
+            for (int offset = 1; offset < 7; offset++)
             {
-                for (int j = 0; j < msGame.Width; j++)
+                // Iterate through all possible effectiveTileNumbers (1-7)
+                for (int effectiveTileNumber = 1; effectiveTileNumber <= 7; effectiveTileNumber++)
                 {
-                    // Find effective ones
-                    Tile currentTile = msGame.Get(i, j);
-                    if (currentTile.EffectiveNumber == 1)
+                    Boolean foundTile = false;
+
+                    // Iterate over gamefield
+                    if (effectiveTileNumber + offset <= 8)
                     {
-                        // See if there are only 2 unclicked tiles
-                        Tile[] surroundingTiles = GetSurroundingTiles(i, j);
-                        if (CountSurroundingUnclickedTiles(surroundingTiles) == 2)
+                        for (int i = 0; i < msGame.Height; i++)
                         {
-                            // If so, add those tiles to pool
-                            foreach (Tile tile in surroundingTiles)
+                            for (int j = 0; j < msGame.Width; j++)
                             {
-                                if (tile.Type == TileType.Unclicked) { tilePool[tileCount++] = tile; }
+                                // Find effective numbered tile
+                                Tile currentTile = msGame.Get(i, j);
+                                if (currentTile.EffectiveNumber == effectiveTileNumber)
+                                {
+                                    // Check if correct amount of surrounding unclicked tiles
+                                    Tile[] surroundingTiles = GetSurroundingTiles(i, j);
+                                    if (CountSurroundingUnclickedTiles(surroundingTiles) == effectiveTileNumber + offset)
+                                    {
+                                        foundTile = true;
+                                        // If so, add those tiles to pool
+                                        foreach (Tile tile in surroundingTiles)
+                                        {
+                                            if (tile.Type == TileType.Unclicked) { tilePool[tileCount++] = tile; }
 
-                            }                           
+                                        }
+                                    }
+                                }
+                            }
                         }
-                    }
-                }
-            }
 
-            // After loop, click random tile from pool
 
-            if (tileCount > 0)
-            {
-                Random rng = new Random();
-                Tile luckyTile = tilePool[rng.Next(0, tileCount)];
-                Mouse.MoveToAndClick(luckyTile.X, luckyTile.Y, MouseButton.Left);
-                msGame.Update();
-            }
-
-            msGame.State = State.Simple;
+                        // After loop, click random tile from pool (if found tile to click)
+                        if (foundTile)
+                        {
+                            Random rng = new Random();
+                            Tile luckyTile = tilePool[rng.Next(0, tileCount)];
+                            Mouse.MoveToAndClick(luckyTile.X, luckyTile.Y, MouseButton.Left);
+                            msGame.Update();
+                            msGame.State = State.Simple;
+                            return;
+                        }
+                    }                  
+                }                  
+            }          
         }
     }
 }
